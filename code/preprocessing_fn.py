f2e24516521b1114802806f3acea95afae31ea14
jinnk0 <jin8073@gmail.com>
2024-10-11 11:04:26 +0900

    feat: target 위치로 클러스터링하고 density와 distance to centroid 변수 추가

--- code/preprocessing_fn.py
+++ code/preprocessing_fn.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.cluster import KMeans
+
+from utils.constant_utils import Config
 
 
 def create_temporal_feature(df: pd.DataFrame)-> pd.DataFrame:
@@ -68,3 +71,45 @@ def standardization(train_data: pd.DataFrame, valid_data: pd.DataFrame, test_dat
 
     return train_data_scaled, valid_data_scaled, test_data_scaled
 
+def create_cluster_density(train_data: pd.DataFrame, valid_data: pd.DataFrame, test_data: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    # 클러스터별 밀도 계산 (클러스터별 포인트 수)
+    cluster_density = train_data.groupby('cluster').size().reset_index(name='density')
+
+    train_data = train_data.merge(cluster_density, on='cluster', how='left')
+    valid_data = valid_data.merge(cluster_density, on='cluster', how='left')
+    test_data = test_data.merge(cluster_density, on='cluster', how='left')
+
+    return train_data, valid_data, test_data
+
+def create_cluster_distance_to_centroid(data: pd.DataFrame, centroids) -> pd.DataFrame:
+    # 포함되는 군집의 centroid와의 거리 계산
+    lat_centroids = np.array([centroids[cluster, 0] for cluster in data['cluster']])
+    lon_centroids = np.array([centroids[cluster, 1] for cluster in data['cluster']])
+    lat_diff = data['latitude'].values - lat_centroids
+    lon_diff = data['longitude'].values - lon_centroids
+    data['distance_to_centroid'] = np.sqrt(lat_diff ** 2 + lon_diff ** 2)
+    return data
+
+def create_clustering_target(train_data: pd.DataFrame, valid_data: pd.DataFrame, test_data: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    # K-means 클러스터링
+    k = 20
+    kmeans = KMeans(n_clusters=k, random_state=Config.RANDOM_SEED)
+    train_data['cluster'] = kmeans.fit_predict(train_data[['latitude', 'longitude']])
+    valid_data['cluster'] = kmeans.predict(valid_data[['latitude', 'longitude']])
+    test_data['cluster'] = kmeans.predict(test_data[['latitude', 'longitude']])
+    
+    train_data['cluster'] = train_data['cluster'].astype('category')
+    valid_data['cluster'] = valid_data['cluster'].astype('category')
+    test_data['cluster'] = test_data['cluster'].astype('category')
+
+    # 군집 밀도 변수 추가
+    train_data, valid_data, test_data = create_cluster_density(train_data, valid_data, test_data)
+
+    centroids = kmeans.cluster_centers_
+
+    # 군집 centroid까지의 거리 변수 추가
+    train_data = create_cluster_distance_to_centroid(train_data, centroids)
+    valid_data = create_cluster_distance_to_centroid(valid_data, centroids)
+    test_data = create_cluster_distance_to_centroid(test_data, centroids)
+
+    return train_data, valid_data, test_data
