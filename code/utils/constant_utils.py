dfa8986f5393c794a0794c7e3051866ddd96c439
Park jaeuk <jwpark3639@gmail.com>
2024-10-16 07:48:50 +0000

    remove: delete unused funtion

--- code/utils/constant_utils.py
+++ code/utils/constant_utils.py
@@ -13,6 +13,6 @@ class Directory:
     interest_rate = pd.read_csv(os.path.join(root_path, "data/interestRate.csv"))
     park_info = pd.read_csv(os.path.join(root_path, "data/parkInfo.csv"))
     school_info = pd.read_csv(os.path.join(root_path, "data/schoolinfo.csv"))
-    subway = pd.read_csv(os.path.join(root_path, "data/subwayInfo.csv"))
+    subway_info = pd.read_csv(os.path.join(root_path, "data/subwayInfo.csv"))
 
     result_path = os.path.join(root_path, 'result')
