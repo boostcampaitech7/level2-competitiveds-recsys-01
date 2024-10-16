0be5985508f4a849707828e0b1b2cd269f91603d
Park jaeuk <jwpark3639@gmail.com>
2024-10-16 10:37:33 +0000

    feat: adding train case

--- code/features.py
+++ code/features.py
@@ -272,60 +272,63 @@ def distance_gangnam(df):
     return df
 
 
-def create_temporal_feature(df: pd.DataFrame)-> pd.DataFrame:
-    df_preprocessed = df.copy()
+def create_temporal_feature(train_data: pd.DataFrame, valid_data: pd.DataFrame, test_data: pd.DataFrame)-> pd.DataFrame:
+    def combination_temporal_feature(df):
+        df_preprocessed = df.copy()
+        
+        df_preprocessed['year'] = df_preprocessed['contract_year_month'].astype(str).str[:4].astype(int)
+        df_preprocessed['month'] = df_preprocessed['contract_year_month'].astype(str).str[4:].astype(int)
+        df_preprocessed['date'] = pd.to_datetime(df_preprocessed['year'].astype(str) + df_preprocessed['month'].astype(str).str.zfill(2) + df_preprocessed['contract_day'].astype(str).str.zfill(2))
+        # 기본 특성 생성 (모든 데이터셋에 동일하게 적용 가능)
+        df_preprocessed['day_of_week'] = df_preprocessed['date'].dt.dayofweek
+        #df['is_weekend'] = df['day_of_week'].isin([5, 6]).astype(int)
+        df_preprocessed['quarter'] = df_preprocessed['date'].dt.quarter
+        df_preprocessed['is_month_end'] = (df_preprocessed['date'].dt.is_month_end).astype(int)
+        df_preprocessed['season'] = df_preprocessed['month'].map({1: 'Winter', 2: 'Winter', 3: 'Spring', 4: 'Spring', 
+                                        5: 'Spring', 6: 'Summer', 7: 'Summer', 8: 'Summer', 
+                                        9: 'Fall', 10: 'Fall', 11: 'Fall', 12: 'Winter'})
+        return df_preprocessed
+    train_data = combination_temporal_feature(train_data)
+    valid_data = combination_temporal_feature(valid_data)
+    test_data = combination_temporal_feature(test_data)
+
+    return train_data, valid_data, test_data
+
+
+
+def create_sin_cos_season(train_data: pd.DataFrame, valid_data: pd.DataFrame, test_data: pd.DataFrame)-> pd.DataFrame:
+    def combination_sin_cos_season(df):
+        df_preprocessed = df.copy()
+        # Cyclical encoding for seasons
+        season_dict = {'Spring': 0, 'Summer': 1, 'Fall': 2, 'Winter': 3}
+        df_preprocessed['season_numeric'] = df_preprocessed['season'].map(season_dict)
+        df_preprocessed['season_sin'] = np.sin(2 * np.pi * df_preprocessed['season_numeric'] / 4)
+        df_preprocessed['season_cos'] = np.cos(2 * np.pi * df_preprocessed['season_numeric'] / 4)
+        df_preprocessed = df_preprocessed.drop(['season_numeric'], axis=1)
+        return df_preprocessed
     
-    df_preprocessed['year'] = df_preprocessed['contract_year_month'].astype(str).str[:4].astype(int)
-    df_preprocessed['month'] = df_preprocessed['contract_year_month'].astype(str).str[4:].astype(int)
-    df_preprocessed['date'] = pd.to_datetime(df_preprocessed['year'].astype(str) + df_preprocessed['month'].astype(str).str.zfill(2) + df_preprocessed['contract_day'].astype(str).str.zfill(2))
+    train_data = combination_sin_cos_season(train_data)
+    valid_data = combination_sin_cos_season(valid_data)
+    test_data = combination_sin_cos_season(test_data)
 
-    # 기본 특성 생성 (모든 데이터셋에 동일하게 적용 가능)
-    df_preprocessed['day_of_week'] = df_preprocessed['date'].dt.dayofweek
-    #df['is_weekend'] = df['day_of_week'].isin([5, 6]).astype(int)
-    df_preprocessed['quarter'] = df_preprocessed['date'].dt.quarter
-    df_preprocessed['is_month_end'] = (df_preprocessed['date'].dt.is_month_end).astype(int)
-    df_preprocessed['season'] = df_preprocessed['month'].map({1: 'Winter', 2: 'Winter', 3: 'Spring', 4: 'Spring', 
-                                    5: 'Spring', 6: 'Summer', 7: 'Summer', 8: 'Summer', 
-                                    9: 'Fall', 10: 'Fall', 11: 'Fall', 12: 'Winter'})
-    return df_preprocessed
+    return train_data, valid_data, test_data
 
 
-def create_sin_cos_season(df: pd.DataFrame)-> pd.DataFrame:
-    df_preprocessed = df.copy()
-    # Cyclical encoding for seasons
-    season_dict = {'Spring': 0, 'Summer': 1, 'Fall': 2, 'Winter': 3}
-    df_preprocessed['season_numeric'] = df_preprocessed['season'].map(season_dict)
-    df_preprocessed['season_sin'] = np.sin(2 * np.pi * df_preprocessed['season_numeric'] / 4)
-    df_preprocessed['season_cos'] = np.cos(2 * np.pi * df_preprocessed['season_numeric'] / 4)
+def create_floor_area_interaction(train_data: pd.DataFrame, valid_data: pd.DataFrame, test_data: pd.DataFrame) -> pd.DataFrame:
+    def floor_weighted(df):
+        df_preprocessed = df.copy()
+        df_preprocessed['floor_weighted'] = df_preprocessed['floor'].apply(lambda x: x if x >= 0 else x * -0.5)
+        df_preprocessed['floor_area_interaction'] = (
+            df_preprocessed['floor_weighted'] * df_preprocessed['area_m2']
+        )
+        df_preprocessed.drop(['floor_weighted'], axis = 1, inplace = True)
+        return df_preprocessed
+    
+    train_data = floor_weighted(train_data)
+    valid_data = floor_weighted(valid_data)
+    test_data = floor_weighted(test_data)
 
-    df_preprocessed = df_preprocessed.drop(['season_numeric'], axis=1)
-    return df_preprocessed
-
-
-
-def create_sin_cos_season(df: pd.DataFrame)-> pd.DataFrame:
-    df_preprocessed = df.copy()
-    # Cyclical encoding for seasons
-    season_dict = {'Spring': 0, 'Summer': 1, 'Fall': 2, 'Winter': 3}
-    df_preprocessed['season_numeric'] = df_preprocessed['season'].map(season_dict)
-    df_preprocessed['season_sin'] = np.sin(2 * np.pi * df_preprocessed['season_numeric'] / 4)
-    df_preprocessed['season_cos'] = np.cos(2 * np.pi * df_preprocessed['season_numeric'] / 4)
-
-    df_preprocessed = df_preprocessed.drop(['season_numeric'], axis=1)
-    return df_preprocessed
-
-
-def create_floor_area_interaction(df: pd.DataFrame) -> pd.DataFrame:
-    df_preprocessed = df.copy()
-
-    df_preprocessed['floor_weighted'] = df_preprocessed['floor'].apply(lambda x: x if x >= 0 else x * -0.5)
-    df_preprocessed['floor_area_interaction'] = (
-        df_preprocessed['floor_weighted'] * df_preprocessed['area_m2']
-    )
-    df_preprocessed.drop(['floor_weighted'], axis = 1, inplace = True)
-    return df_preprocessed
-
-from model import *
+    return train_data, valid_data, test_data
 
 
 
--- code/inference.py
+++ code/inference.py
@@ -6,12 +6,21 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 from utils.constant_utils import Directory
 
 def inference(model, type, X, y=None):
-    if type=='validation':
+    if type=='train':
         prediction = model.predict(X)
         mae = mean_absolute_error(y, prediction)
+        print("Train Mae score")
+        print(mae)
         return prediction, mae
-    prediction = model.predict(X)
-    sample_submission = Directory.sample_submission
-    sample_submission['deposit'] = prediction
-
-    return sample_submission
+    
+    elif type=='validation':
+        prediction = model.predict(X)
+        mae = mean_absolute_error(y, prediction)
+        print("validation Mae score")
+        print(mae)
+        return prediction, mae
+    else:
+        prediction = model.predict(X)
+        sample_submission = Directory.sample_submission
+        sample_submission['deposit'] = prediction
+        return sample_submission
