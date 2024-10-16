1cf73e38be23fae23f8517c322f046932d4c3e43
Park jaeuk <jwpark3639@gmail.com>
2024-10-16 07:46:18 +0000

    remove: delete unused function

--- code/features.py
+++ code/features.py
@@ -5,6 +5,45 @@ import numpy as np
 from sklearn.neighbors import KDTree
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
+def create_temporal_feature(df: pd.DataFrame)-> pd.DataFrame:
+    df_preprocessed = df.copy()
+    
+    df_preprocessed['year'] = df_preprocessed['contract_year_month'].astype(str).str[:4].astype(int)
+    df_preprocessed['month'] = df_preprocessed['contract_year_month'].astype(str).str[4:].astype(int)
+    df_preprocessed['date'] = pd.to_datetime(df_preprocessed['year'].astype(str) + df_preprocessed['month'].astype(str).str.zfill(2) + df_preprocessed['contract_day'].astype(str).str.zfill(2))
+
+    # 기본 특성 생성 (모든 데이터셋에 동일하게 적용 가능)
+    df_preprocessed['day_of_week'] = df_preprocessed['date'].dt.dayofweek
+    df_preprocessed['is_weekend'] = df_preprocessed['day_of_week'].isin([5, 6]).astype(int)
+    df_preprocessed['quarter'] = df_preprocessed['date'].dt.quarter
+    df_preprocessed['is_month_end'] = (df_preprocessed['date'].dt.is_month_end).astype(int)
+    df_preprocessed['season'] = df_preprocessed['month'].map({1: 'Winter', 2: 'Winter', 3: 'Spring', 4: 'Spring', 
+                                    5: 'Spring', 6: 'Summer', 7: 'Summer', 8: 'Summer', 
+                                    9: 'Fall', 10: 'Fall', 11: 'Fall', 12: 'Winter'})
+    return df_preprocessed
+
+def create_sin_cos_season(df: pd.DataFrame)-> pd.DataFrame:
+    df_preprocessed = df.copy()
+    # Cyclical encoding for seasons
+    season_dict = {'Spring': 0, 'Summer': 1, 'Fall': 2, 'Winter': 3}
+    df_preprocessed['season_numeric'] = df_preprocessed['season'].map(season_dict)
+    df_preprocessed['season_sin'] = np.sin(2 * np.pi * df_preprocessed['season_numeric'] / 4)
+    df_preprocessed['season_cos'] = np.cos(2 * np.pi * df_preprocessed['season_numeric'] / 4)
+
+    df_preprocessed = df_preprocessed.drop(['season_numeric'], axis=1)
+    return df_preprocessed
+
+
+def create_floor_area_interaction(df: pd.DataFrame) -> pd.DataFrame:
+    df_preprocessed = df.copy()
+
+    df_preprocessed['floor_weighted'] = df_preprocessed['floor'].apply(lambda x: x if x >= 0 else x * -0.5)
+    df_preprocessed['floor_area_interaction'] = (
+        df_preprocessed['floor_weighted'] * df_preprocessed['area_m2']
+    )
+    df_preprocessed.drop(['floor_weighted'], axis = 1, inplace = True)
+    return df_preprocessed
+
 def clustering(total_df, info_df, feat_name, n_clusters=20):
     info = info_df[['longitude', 'latitude']].values
     
