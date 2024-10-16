a4fc83affd32b812c9a9ba9065b8dd3b011edfb1
Park jaeuk <jwpark3639@gmail.com>
2024-10-16 06:26:11 +0000

    feat: add train_valid_concat function

--- code/utils/common_utils.py
+++ code/utils/common_utils.py
@@ -20,6 +20,17 @@ def merge_data(train_data, test_data):
     df = df.merge(interest_rate, on='contract_year_month', how='left')
     return df
 
+def merge_data(train_data, test_data):
+    train_data['type'] = 'train'
+    test_data['type'] = 'test'
+
+    df = pd.concat([train_data, test_data], axis = 0)
+    df.drop(['index'], axis = 1, inplace = True)
+
+    interest_rate = Directory.interest_rate.rename(columns = {'year_month' : 'contract_year_month'})
+    df = df.merge(interest_rate, on='contract_year_month', how='left')
+    return df
+
 
 def train_valid_test_split(df):
     # 데이터 분할
@@ -45,6 +56,11 @@ def split_feature_target(train_data_scaled, valid_data_scaled, test_data_scaled)
     
     return X_train, y_train, X_valid, y_valid, X_test
 
+def train_valid_concat(X_train, X_valid, y_train, y_valid):
+    X_total, y_total = pd.concat([X_train, X_valid]), pd.concat([y_train, y_valid])
+    return X_total, y_total
+
+
 
 def submission_to_csv(submit_df, file_name):
     submission_path = os.path.join(Directory.result_path, "submission")
@@ -63,3 +79,5 @@ def mae_to_csv(mae_df, file_name):
     file_name += '_' + time.strftime('%x', time.localtime())[:5].replace('/','') + '.csv'
     mae_file_path = os.path.join(mae_path, file_name)
     mae_df.to_csv(mae_file_path, index=False, encoding='utf-8-sig')
+
+
