906c860ec863962c6160bd940a8463338598949e
choitaesoon <cts0817@naver.com>
2024-10-18 12:31:17 +0300

    test ensemble, optuna, feature importance

--- code/utils/constant_utils.py
+++ code/utils/constant_utils.py
@@ -5,8 +5,49 @@ class Config:
     RANDOM_SEED = 42
     SPLIT_YEAR_START = 202307
     SPLIT_YEAR_END = 202312
-    CATEGORICAL_COLUMNS = ['contract_type']
+    CATEGORICAL_COLUMNS = ['contract_type','cluster','subway_info','park_info', 'school_info', 'distance_category','age_category', 'floor_category', 'area_category']
     NUMERICAL_COLUMNS = ['area_m2','contract_year_month','contract_day','floor','built_year','latitude','longitude','age','deposit']
+
+    # RandomForest Regressor Parameters
+    RANDOM_FOREST_PARAMS = {
+        'n_estimators': 1000,
+        'max_depth': None,
+        'min_samples_split': 2,
+        'min_samples_leaf': 1,
+        'bootstrap': True,
+        'random_state': RANDOM_SEED,
+        'n_jobs': -1 
+    }
+    
+    # LightGBM Regressor Parameters
+    LIGHT_GBM_PARAMS = {
+        'learning_rate': 0.05,
+        'n_estimators': 1000,
+        'max_depth': -1,
+        'num_leaves': 31, 
+        'min_child_samples': 20, 
+        'subsample': 0.8,
+        'colsample_bytree': 0.8, 
+        'random_state': RANDOM_SEED,
+        'n_jobs': -1 
+    }
+    
+    # XGBoost Regressor Parameters
+    XGBOOST_PARAMS = {'learning_rate' : 0.3,
+                      'n_estimators' : 1000,
+                      'enable_categorical' : True,
+                      'random_state' : RANDOM_SEED
+                      }
+    
+    # CatBoost Regressor Paramters
+    CATBOOST_PARAMS = {'iterations': 1000,
+                       'learning_rate': 0.1,
+                       'depth': 6,
+                       'loss_function': 'MAE',
+                       'random_seed': RANDOM_SEED,
+                       'cat_features': CATEGORICAL_COLUMNS,
+                       'verbose': 100              
+}
 
     
 class Directory:
